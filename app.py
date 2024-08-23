d38004c4b711b0d91233cd6810c0fd846430d988
GOLDY KHATRI <goldykhatri@GOLDYs-MacBook-Air.local>
2024-08-24 00:37:08 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title('Kid Molester')
+
+col1,col2 = st.columns(2)
+with col1:
+    st.image('Faculty_of_mathematical_sciences,_University_of_Delhi.jpg')
+    with col2:
+        st.write(""""
+        This site is used to created for killing of innocent children.
+        """)
+
