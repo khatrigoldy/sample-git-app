8bc18b05d3615abb016ca453b0bf658d5545e780
GOLDY KHATRI <goldykhatri@GOLDYs-MacBook-Air.local>
2024-08-24 17:53:24 +0530

    add new image

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.title('Kid Molester')
 
 col1,col2 = st.columns(2)
 with col1:
-    st.image('Faculty_of_mathematical_sciences,_University_of_Delhi.jpg')
+    st.image('peakpx-5.jpg')
     with col2:
         st.write(""""
         This site is used to created for killing of innocent children.
